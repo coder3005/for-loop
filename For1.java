80acbf616ae53b4fd3586bc21d51a30a79a6cdcf
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 17:02:02 +0530

    Create For1.java

---
+++ For1.java
@@ -0,0 +1,11 @@
+/* for Loop Example...! */
+class For
+{
+    public static void main(String[] args) {
+
+        for (int i=10; i>=1; i--)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
